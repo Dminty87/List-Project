dc096b8540b5c5e759495df244da73214e084b6a
Daniel Minty <60455996+Dminty87@users.noreply.github.com>
2024-04-16 13:26:08 -0700

    Update

--- ToDoList/listScript.js
+++ ToDoList/listScript.js
@@ -3,19 +3,55 @@
 
 
 function testScript(){
-    document.getElementById("output").innerHTML = "Success!"
+    document.getElementById("output").innerHTML = "Success! At current time " + new Date()
 }
 
 
 function loadXML(){
-    const req = new XMLHttpRequest();
+    var req = new XMLHttpRequest();
     req.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("listOutput").
+
+            var xmlDoc = req.responseXML
+            
+            //document.documentElement.append(new CDATASection())
+            /*
+            while(xmlStr.includes("ListItem")){
+                xmlStr = xmlStr.replace("ListItem", "li")
+            }
+            */
+
+            var children = xmlDoc.documentElement.children
+            
+            var list = new String()
+
+            for (const x of children)
+            {
+                list += "<li>" + x.textContent + "</li>\n"
+            }
+
+
+
+            document.getElementById("xmlOut").innerText = "xmlOut: Start:\n" + list + ":end"
+
+            document.getElementById("listOutput").innerHTML = list
+            
         }
     }
 
-    req.open("GET", "GeeSkillet.xml", true)
+    req.open("GET", "\\ToDoList\\ToDoList\\bin\\Debug\\ToDoListData\\March 22nd.xml", true)
     req.send()
 }
+
+function testReq(){
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("reqOut").innerHTML = "reqOut: Start:" + req.responseText + ":end"
+        }
+    }
+    req.open("GET", "lorem.txt", true)
+    req.send("testing")
+}
+
 
